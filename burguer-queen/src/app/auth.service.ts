7b85d85b0f331eef10e4e90b770b5273e68b9c53
OnXochitl <xhitl@hotmail.es>
2023-02-20 10:39:43 -0600

    Validacion de inputs

---
+++ burguer-queen/src/app/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface AuthToken {
+  token: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  authUser(email: string, password: string): Observable<AuthToken> {
+    // cambiar a post cuando la api ya este implementada
+    // return this.http.post<AuthToken>('http://localhost:8000/auth', {email, password});
+    return this.http.get<AuthToken>('http://localhost:8000/auth');
+  }
+}
+
+//const sum = (x: number, y: number): number => x + y;  
--- burguer-queen/src/app/login/login.component.ts
+++ burguer-queen/src/app/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService, AuthToken } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  contactForm!: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.initForm();
+  }
+
+  initForm(): FormGroup {
+    return this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]]
+    })
+  }
+  // userEmailControl = new FormGroup('');
+  // constructor(private authService: AuthService) {}
   
+  // logInUser() {
+  //   this.authService.authUser("", "").subscribe((token: AuthToken) => {
+  //     console.log(token);
+  //   });
+  // }
 }
